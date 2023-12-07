7f60fa59d3188a16428b49d6371a8d073c0df326
sullivan <SullivanMuse@users.noreply.github.com>
2023-12-07 07:19:27 -0600

    Initial commit

---
+++ OpenGLTemplate/headers/core.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include "engine/window.h"
+#include "engine/input.h"
+#include "engine/shader.h"
+#include "engine/buffers.h"
+#include "engine/data.h"
